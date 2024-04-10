14586c4a0570c5b50408f207084d2ee846f91e02
joelle78 <96306220+joelle78@users.noreply.github.com>
2024-04-10 15:14:22 +0200

    Set up project
    - Main CSS
    - Homepage
    - Primary button

--- massage-praktijk/app.vue
+++ massage-praktijk/app.vue
@@ -1,5 +1,10 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <link rel="stylesheet" href="https://use.typekit.net/rtt3tac.css">
+    <NuxtLayout>
+      <NuxtPage/>
+    </NuxtLayout>
   </div>
 </template>
+<script setup lang="ts">
+</script>
