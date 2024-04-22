02ad0dfa85ae98fef1cffebc9a65c692b0926f53
joelle78 <96306220+joelle78@users.noreply.github.com>
2024-04-22 12:30:57 +0200

    Sportmassage pagina
    - Section three
    - Section Four

---
+++ massage-praktijk/components/massages/sportmassage/SectionTwo.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <section>
+    <h2>Wat is een</h2>
+    <h2><span>Sport massage?</span></h2>
+    <p>Een sportmassage is een specifieke massagebehandeling die zich richt op het verbeteren van de doorbloeding, het
+      verminderen van spierspanning en het versnellen van het herstelproces. Door gerichte technieken en bewegingen
+      worden de spieren voorbereid op inspanning en worden na het sporten afvalstoffen afgevoerd, waardoor spierpijn
+      wordt verminderd en het herstel wordt bevorderd. </p>
+  </section>
+</template>
+
+<style scoped>
+section {
+  margin-top: 2rem;
+  margin-left: 1.5rem;
+}
+
+span {
+  color: var(--secondary);
+  font-family: bebas-neue-pro, sans-serif;
+}
+
+p {
+  margin: 2rem 0 2rem 0;
+}
+</style>
