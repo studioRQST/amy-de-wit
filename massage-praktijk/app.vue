b461432a9cf3f6b739e2f4c214e194f70a16517b
rosahoffer <rosahoffer@live.nl>
2024-04-23 19:13:20 +0200

    whatsapp button push

--- massage-praktijk/app.vue
+++ massage-praktijk/app.vue
@@ -3,8 +3,49 @@
     <link rel="stylesheet" href="https://use.typekit.net/rtt3tac.css">
     <NuxtLayout>
       <NuxtPage/>
+        <nuxt-link class="whatsapp-wrapper" href="https://wa.me/610629690" aria-label="Stuur Amy een Whatsapp!">
+          <div class="whatsapp-background">
+            <div class="whatsapp-image">
+              <img alt="WhatsApp Icoon" src="/static/icons/icon-whatsapp.svg"/>
+            </div>
+          </div> 
+        </nuxt-link>
     </NuxtLayout>
   </div>
 </template>
-<script setup lang="ts">
-</script>
+
+<style scoped>
+
+.whatsapp-wrapper{
+  width: 4rem;
+  height: 4rem;
+  position: fixed;
+  top: auto;
+  bottom: 2rem;
+  left: auto;
+  right: 2rem;
+  transition: transform 0.3s ease-in-out;
+}
+
+.whatsapp-background{
+  width: 100%;
+  height: 100%;
+  background-color: #32D951;
+  border-radius: 100%;
+  transition: transform 0.2s ease-in-out;
+}
+
+.whatsapp-image {
+  width: 100%;
+  height: 100%;
+  justify-content: center;
+  align-items: center;
+  display: flex;
+}
+
+.whatsapp-wrapper:hover .whatsapp-background {
+  transform: scale(1.1);
+  box-shadow: 0 0 10px var(--secondary); /* Voeg een lichte schaduw toe */
+}
+
+</style>
