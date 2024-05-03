6cbf1f60ddc334b54102c045dc83b0b04e882ca7
rosahoffer <rosahoffer@live.nl>
2024-05-03 20:07:21 +0200

    added SEO

--- massage-praktijk/pages/massage/voetreflex.vue
+++ massage-praktijk/pages/massage/voetreflex.vue
@@ -8,10 +8,10 @@
       <title>Voetreflexmassage voor Welzijn | Massagepraktijk Amy de Wit</title>
       <meta name="description" content="Ervaar de helende werking van voetreflexmassage bij Massagepraktijk Amy de Wit, onder leiding van gepassioneerde masseur Amy de Wit. Ontdek hoe deze therapeutische behandeling jou kan helpen bij het bevorderen van ontspanning, balans en welzijn. Maak een afspraak in Hem voor een op maat gemaakte sessie.">
     </head>
-  <NavbarHeader/>
-  <MassagesVoetreflexmassage/>
-  <MassagesContactMassages/>\
-  <Footer/>
+    <NavbarHeader/>
+    <MassagesVoetreflexmassage/>
+    <MassagesContactMassages/>\
+    <Footer/>
   </div>
 </template>
 
