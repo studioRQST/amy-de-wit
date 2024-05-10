01030d4ea372871f84bebd205dbb8cca5b83306f
joelle78 <96306220+joelle78@users.noreply.github.com>
2024-05-10 13:45:15 +0200

    404 page
    - mobile

---
+++ massage-praktijk/pages/404.vue
@@ -0,0 +1,60 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <section>
+    <div class="container-header">
+        <h1>404 error</h1>
+        <h2>Pagina niet gevonden</h2>
+    </div>
+    <div class="container-par">
+        <h3>Het lijkt erop dat je een verkeerde afslag hebt genomen!</h3>
+        <p>De pagina die je probeert te bereiken, bestaat niet (meer) of is verplaatst. Maar geen zorgen, ik sta voor je
+          klaar om je weer op het juiste spoor te zetten.</p>
+    </div>
+        <nuxt-link href="/" aria-label="Link naar home page">
+          <button class="primary-button">
+                <span class="circle" aria-hidden="true">
+                <span class="icon arrow"></span>
+                </span>
+            <span class="button-text">Terug naar start</span>
+          </button>
+        </nuxt-link>
+
+  </section>
+</template>
+
+<style scoped>
+section {
+  margin-top: 5rem;
+  margin-left: 1rem;
+
+}
+
+h2 {
+  color: var(--secondary);
+}
+
+
+
+.container-header {
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+  margin-bottom: 1rem;
+}
+
+.container-par {
+  display: flex;
+  flex-direction: column;
+  gap: 1rem
+}
+
+button {
+  margin-top: 2rem;
+}
+
+
+
+</style>
