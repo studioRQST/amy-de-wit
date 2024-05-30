81efd9bf37c5efb147245e7156612c2def145dae
rosahoffer <rosahoffer@live.nl>
2024-05-30 21:31:00 +0200

    seo update index page

--- massage-praktijk/pages/index.vue
+++ massage-praktijk/pages/index.vue
@@ -1,88 +1,27 @@
 <template>
   <div>
-    <NavbarHeader />
-    <HomePage />
-    <FooterNgs />
-    <Footer />
+    <head>
+      <title>Verlicht Herstel & Vernieuw - Massagepraktijk Amy de Wit</title>
+      <meta name="description" content="Ontdek de kracht van lymfedrainage en sportmassage voor optimaal herstel bij Massagepraktijk Amy de Wit. Laat mij je begeleiden op een reis van herstel en vernieuwing in Hem.">
+      <meta name="keywords" content="lymfedrainage, sportmassage, voetreflex, Amy de Wit, massage, massagepraktijk">
+
+      <!-- Facebook-pagina -->
+      <meta property="og:url" content="https://www.facebook.com/p/Massagepraktijk-Amy-de-Wit-100068390764954/?locale=nl_NL" />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content="Massagepraktijk Amy de Wit" />
+      <meta property="og:description" content="Massagepraktijk Amy de Wit biedt lymfedrainage, sportmassage en meer voor optimaal herstel en vernieuwing." />
+      <meta property="og:image" content="https://www.amydewit.nl/_nuxt/sportmassage-3.D4JtxzRM.avif" />
+
+      <!-- Adres -->
+      <meta name="address" content="Elbaweg 30, 1607 HEM, Nederland" />
+      
+      <!-- Openingstijden -->
+      <meta property="openingHours" content="Dinsdag 09:00-14:00 en 19:30-21:00; Woensdag 10:45-14:00 en 19:30-21:00; Donderdag 09:00-14:00" />
+
+    </head>
+    <NavbarHeader/>
+    <HomePage/>
+    <FooterNgs/>
+    <Footer/>
   </div>
 </template>
-
-<script setup>
-definePageMeta({
-  title: 'Verlicht Herstel & Vernieuw - Massagepraktijk Amy de Wit',
-  meta: [
-    {
-      name: 'description',
-      content: 'Ontdek de kracht van lymfedrainage en sportmassage voor optimaal herstel bij Massagepraktijk Amy de Wit. Laat mij je begeleiden op een reis van herstel en vernieuwing in Hem.'
-    },
-    {
-      name: 'keywords',
-      content: 'lymfedrainage, sportmassage, voetreflex, Amy de Wit, massage, massagepraktijk'
-    }
-  ],
-  link: [
-    {
-      rel: 'canonical',
-      href: 'https://www.amydewit.nl'
-    }
-  ],
-  script: [
-    {
-      type: 'application/ld+json',
-      innerHTML: `
-      {
-        "@context": "https://schema.org",
-        "@type": "LocalBusiness",
-        "name": "Massagepraktijk Amy de Wit",
-        "image": "https://www.amydewit.nl/_nuxt/sportmassage-3.D4JtxzRM.avif",
-        "@id": "https://www.amydewit.nl",
-        "url": "https://www.amydewit.nl",
-        "telephone": "+31610629690",
-        "address": {
-          "@type": "PostalAddress",
-          "streetAddress": "Elbaweg 30",
-          "addressLocality": "Hem",
-          "postalCode": "1607 MP",
-          "addressCountry": "NL"
-        },
-        "openingHoursSpecification": [
-          {
-            "@type": "OpeningHoursSpecification",
-            "dayOfWeek": "Tuesday",
-            "opens": "09:00",
-            "closes": "14:00"
-          },
-          {
-            "@type": "OpeningHoursSpecification",
-            "dayOfWeek": "Tuesday",
-            "opens": "19:30",
-            "closes": "21:00"
-          },
-          {
-            "@type": "OpeningHoursSpecification",
-            "dayOfWeek": "Wednesday",
-            "opens": "10:45",
-            "closes": "14:00"
-          },
-          {
-            "@type": "OpeningHoursSpecification",
-            "dayOfWeek": "Wednesday",
-            "opens": "19:30",
-            "closes": "21:00"
-          },
-          {
-            "@type": "OpeningHoursSpecification",
-            "dayOfWeek": "Thursday",
-            "opens": "09:00",
-            "closes": "14:00"
-          }
-        ],
-        "sameAs": [
-          "https://www.facebook.com/p/Massagepraktijk-Amy-de-Wit-100068390764954/"
-        ]
-      }
-      `
-    }
-  ]
-})
-</script>
